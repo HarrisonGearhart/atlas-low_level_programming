3e596d7fe481349962bbd6cb17f5fa7b7c55265d
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-16 14:03:24 -0500

    file_6

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("The size of an int: %lu byte(s)\n", (unsignged long)sizeof(b));
+	printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("The size of a long long int: %lu byte(s)/n", (unsigned long)sizeof(d));
+	printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
+}
