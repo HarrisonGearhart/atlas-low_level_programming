9e0702b0da760b69c4f75ad13803058d97189f21
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-18 00:11:42 -0500

    file_8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0; n <= 9; n++)
+		putchar(d);
+	for (l = 'a'; l <='f'; l++)
+		putchar('l');
+	putchar('\n');
+} 
