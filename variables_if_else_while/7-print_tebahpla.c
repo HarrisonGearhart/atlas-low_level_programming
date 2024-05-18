3a2ba5a101064912fe7ed058d8051e3bcc0ab948
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-17 23:29:32 -0500

    file_7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  *
  * main - Entry point
@@ -11,8 +12,10 @@
 int main(void)
 {
 	char low; 
+	
 	for (low = 'z'; low >= 'a'; low--)
 		putchar(low);
 		putchar('\n');
+	
 	return (0);
 }
