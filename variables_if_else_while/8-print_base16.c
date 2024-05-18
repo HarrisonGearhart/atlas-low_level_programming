be333783a9ad6ad7145d65ad4c94b39b4fe65858
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-18 00:18:39 -0500

    file_8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -14,15 +14,11 @@ int main(void)
 	int n;
 	char l;
 
-	for (n = 0; n <= 9; n++)
-	{	
+	for (n = 0; n <= 9; n++)	
 		putchar(n);
-	}
 	for (l = 'a'; l <='f'; l++)
-	{	
 		putchar(l);
-	}
 	putchar('\n');
 
 	return (0);
-} 
+}
