cd4299241123a4c94b4a32de2de6e04fae85ff3f
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-20 11:35:03 -0500

    file_1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: print alphabet in lowercase, followed by new line
+ * Return: (void)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a'
+
+	while (c <= 'z');
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
