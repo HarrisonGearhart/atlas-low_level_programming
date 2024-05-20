75430dc61173c4790e8ce4493d0cfe2b60d73bd1
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-20 13:41:01 -0500

    file_6

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -14,13 +14,15 @@
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+	} 
+	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);;
-	} else if (n < 0)
+		return (0);
+	}
+	 else if (n < 0)
 	{
-		_putchar('-');;
+		_putchar('-');
 	}
 		return (-1);
 }
