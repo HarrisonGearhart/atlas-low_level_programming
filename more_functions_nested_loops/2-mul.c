2df6c950166eae341033a531a90ece5df00e70cb
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-22 22:59:10 -0500

    file_2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -4,10 +4,10 @@
  * mul(int a, int b) - multiplies int a and int b
  * @a: first integer being multiplied
  * @b: second integer being mulitplied
- * Return: result of a * b 
+ * Return: result of a * b
 */
 
-int mul (int a, int b)
+int mul(int a, int b)
 
 {
 	return ((a * b));
