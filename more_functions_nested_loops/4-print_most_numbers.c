05b58171ae658291ee81383d83825342169477be
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-22 23:47:32 -0500

    file_3

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,16 @@
  * Return: void
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 
 {
 	int n;
 
 	for (n = 0; n <= 9; n++)
 	{
-		_putchar(n + '0');
-	}
-		_putchar('\n');
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+	_putchar('\n');
 }
