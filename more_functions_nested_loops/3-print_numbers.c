be020573e0beb0ec259de4a7ef1b3669715bcba7
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-22 23:09:50 -0500

    file_3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9, followed by a new line
+ * @n: numbers being printed
+ * Return: Always 0 (Successful)
+*/
+
+void print_numbers(void)
+
+{
+	int n;
+
+	while (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+		_putchar('\n');
+	}
+}
