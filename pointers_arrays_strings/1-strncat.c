b3536d1e4d560479608f3f69c98523d078db628d
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-31 11:36:22 -0500

    file_1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -7,13 +7,13 @@
  * @n: number of elements to concatenate in
  * Return: pointer to resulting string 'dest'
 */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, c;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
+
 	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
 	{
 		dest[i] = src[c];
