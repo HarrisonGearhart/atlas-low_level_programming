2b49009c989c67a2d92191b99350fd0d09534d68
HarrisonGearhart <hgearhart98@gmail.com>
2024-05-31 11:28:08 -0500

    file_1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting string 'dest'
+*/
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int i, c, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+	{
+		dest[i] = src[c];
+	}
+	return (dest);
+}
