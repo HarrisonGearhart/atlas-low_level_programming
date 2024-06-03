ca7fc64b9f060e19afbddee7af7211d0c059eee2
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-03 13:39:06 -0500

    file_1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy- copies memory
+ * @dest: mem area to copy to
+ * @src: the source to copy
+ * @n: unsigned int variable
+ * Return: Pointer to dest
+*/
+
+char *_memcpy(char *dest; char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,4 +19,5 @@ char *str_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
