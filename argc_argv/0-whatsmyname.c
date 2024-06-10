855ecdd5a191061eae29b59b7cf80a27e4471efe
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-10 15:41:29 -0500

    file_0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <std.lib>
+/**
+ * main - prints name of the function
+ * @argc: # of arguments 
+ * @argv: pointer that contains the arrays of arguments
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n");
+	return (0);
+}
