de9fc0e80f17df707e9f434ef1328cc3f4f7d152
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-10 11:58:09 -0500

    file_3_attempt1

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,4 +1,21 @@
 #include "main.h"
 /**
- * factorial- returns factorial
+ * factorial- returns factorial of given number
+ * @n: given number
+ * Return: intger value or -1 if n < o
 */
+int factorial(int n)
+{
+    if (n < 0)
+    {
+        return (-1);
+    }
+    if (n > 1)
+    {
+        return (n * factorial (n - 1));
+    }
+    else
+    {
+        return (1);
+    }
+}
