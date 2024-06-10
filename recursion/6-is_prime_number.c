afb361ec51dd666920cb991e3fe6381a3082766f
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-10 17:01:28 -0500

    file_6_basecondition

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_prime_number - Checks if number is prime
+ * @n: number being checked
+ * Return: 1 if prime, 0 if not 
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
