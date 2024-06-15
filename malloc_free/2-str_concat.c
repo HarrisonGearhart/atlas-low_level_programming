56977f2d68d6dae0e0456c07b502b25f7e94a38a
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-15 16:10:56 -0500

    betty

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,32 +8,32 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-    int a = 0;
-    int ab = 0;
-    char *con;
+	int a = 0;
+	int ab = 0;
+	char *con;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
     
-    while (s1[a] != '\0')
-        a++;
-    while (s2[ab] != '\0')
-        ab++;
-    con = malloc(sizeof(char) * (a + ab) + 1 );
-    if (con == NULL)
-        return (NULL);
-    while (s1[a] != '\0')
-    {
-        con[a] = s1[a];
-        a++;
-    }
-    while (s2[ab] != '\0')
-    {
-        con[ab] = s2[ab];
-        a++, ab++;
-    }
-    con[a] = '\0';
-    return (con);
+	while (s1[a] != '\0')
+		a++;
+	while (s2[ab] != '\0')
+		ab++;
+	con = malloc(sizeof(char) * (a + ab) + 1 );
+	if (con == NULL)
+		return (NULL);
+	while (s1[a] != '\0')
+	{
+		con[a] = s1[a];
+		a++;
+	}
+	while (s2[ab] != '\0')
+	{
+		con[ab] = s2[ab];
+		a++, ab++;
+	}
+	con[a] = '\0';
+	return (con);
 }
