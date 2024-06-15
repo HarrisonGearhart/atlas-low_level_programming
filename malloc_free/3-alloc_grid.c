26650fe9c902f454e55c0b8d384f43921fd458e3
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-15 14:57:06 -0500

    file_2_attempt4

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - returns 2 dimensional array of ints
+ * @width:  
+ * @height:
+ * Return: NULL on fail or if w/h is 0. 
+*/
+int **alloc_grid(int width, int height)
+{
+
+}
