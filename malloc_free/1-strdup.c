8da38a8004b68fa89caac605112422ffefb81ec7
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-15 13:27:09 -0500

    file_1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicates string
+ * @str - original string
+ * Return: pointer to duplicated string/NULL if fail
+*/
+char *_strdup(char *str)
+{
+    char *s;
+    int x = 0;
+    int y = 1;
+
+    if (str == NULL)
+     return (NULL);
+    
+    while (str[y])
+    {
+        y++;
+    }
+    s = malloc(sizeof(char) * y);
+    while (x < y)
+    {
+        s[x] = str[x];
+        x++;
+    }
+    return(s);
+}
