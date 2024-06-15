da2691f23ea4402c814c9593ae310f42937dfdf0
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-15 15:50:55 -0500

    betty

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -7,24 +7,24 @@
 */
 char *_strdup(char *str)
 {
-    char *s;
-    int x = 0;
-    int y = 1;
+	char *s;
+	int x = 0;
+	int y = 1;
 
-    if (str == NULL)
-     return (NULL);
-    
-    while (str[y])
-    {
-        y++;
-    }
-    s = malloc((sizeof(char) * y) + 1);
-    if (s == NULL)
-        return (NULL); 
-    while (x < y)
-    {
-        s[x] = str[x];
-        x++;
-    }
-    return(s);
+	if (str == NULL)
+ 	return (NULL);
+
+	while (str[y])
+	{
+		y++;
+	}
+	s = malloc((sizeof(char) * y) + 1);
+	if (s == NULL)
+		return (NULL); 
+	while (x < y)
+	{
+		s[x] = str[x];
+		x++;
+	}
+	return(s);
 }
