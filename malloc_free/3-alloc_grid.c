4d67cb40063cb8a69943a6bd61d513e420b715e6
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-15 16:00:47 -0500

    betty

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -8,38 +8,38 @@
 */
 int **alloc_grid(int width, int height)
 {
-    int **a;
-    int w, x, y, z;
+	int **a;
+	int w, x, y, z;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    a = malloc(sizeof(int *) * height);
-    if (a == NULL)
-    {
-        free (a);
-        return (NULL);
-    }
-    for (w = 0; w < height; w++)
-    {
-            a[w] = malloc(sizeof(int) * width);
+	a = malloc(sizeof(int *) * height);
+	if (a == NULL)
+	{
+		free (a);
+		return (NULL);
+	}
+	for (w = 0; w < height; w++)
+	{
+		a[w] = malloc(sizeof(int) * width);
 
-            if (a[w] == NULL)
-            {
-                for (x = w; x >= 0; x--)
-                {
-                    free (a[x]);
-                }
-                free (a);
-                return (NULL);
-            }
-    }
-    for (y = 0; y < height; y++)
-    {
-        for (z = 0; z < width; z++)
-        {
-            a[y][z] = 0;
-        }
-    }
-    return (a);
+		if (a[w] == NULL)
+		{
+			for (x = w; x >= 0; x--)
+			{
+				free (a[x]);
+			}
+			free (a);
+			return (NULL);
+		}
+	}
+	for (y = 0; y < height; y++)
+	{
+		for (z = 0; z < width; z++)
+		{
+			a[y][z] = 0;
+		}
+	}
+	return (a);
 }
