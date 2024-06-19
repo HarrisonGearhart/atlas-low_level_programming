001b7fb394dbcdb0dd831f82bc413fc25d22dda2
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-19 11:15:26 -0500

    file_2_remove if age NULL

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 		(*d).owner = "(nil)";
 	if ((*d).name == NULL)
 		(*d).name = "(nil)";
-	
+
 	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
 	printf("Owner: %s\n", (*d).owner);
