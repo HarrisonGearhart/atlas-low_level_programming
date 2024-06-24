92b35ca271a30e37bdaff279353811f7061ebb20
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-24 12:38:47 -0500

    function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
-#ifndef main_h
-#define main_h
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdio.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 
