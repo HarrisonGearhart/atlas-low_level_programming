0f081c02bb76f11ef9626f76fcbee225caef839e
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-24 12:33:32 -0500

    file_0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function thats prints name
+ * Return: Void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	if (f == NULL)
+		return;
+
+	f(name);
+}
