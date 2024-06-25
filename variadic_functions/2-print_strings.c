84032318d652001a87f1dbd4d0fb3070068f7905
HarrisonGearhart <hgearhart98@gmail.com>
2024-06-24 22:37:51 -0500

    file_2_attempt2_*

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -11,14 +11,14 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
-	char string;
+	char *string;
 	unsigned int index = 0;
 
 	va_start(strings, n);
 
 	for (index = 0; index < n; index++)
 	{
-		string = va_arg(strings, char);
+		string = va_arg(strings, char *);
 		if (string == NULL)
 			printf("(nil)");
 		else
