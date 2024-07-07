a1c47be1f7ec8f748436caaad44cec689556392f
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-06 22:49:23 -0500

    file_2/attempt1

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add_node - Adds new node in linked list
+ * @head: head of linked list
+ * @str: the string to be added
+ * Return: Address of the new element added to the linked list
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string being counted
+ * Return: the length of the string
+*/
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
