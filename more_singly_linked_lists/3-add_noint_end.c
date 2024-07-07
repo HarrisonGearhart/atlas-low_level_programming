1cec6f011f21a8e14cbf3f6004ec54555e8ad373
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-07 01:00:49 -0500

    file_3/initialization

---
+++ more_singly_linked_lists/3-add_noint_end.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add_noint_end - adds a new node at the end of the singly linked list
+ * @head: head of the list
+ * @n: integer
+ * Return: Address of the new element
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+
+}
