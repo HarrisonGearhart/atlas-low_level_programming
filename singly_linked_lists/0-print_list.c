0c6ce9af63f9848a865e568f44d0a5b4cde1faa6
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-06 22:08:29 -0500

    file_0/firstattempt

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,5 +3,21 @@
 #include <stdio.h>
 /**
  * print_list - prints the elements of a linked list
+ * @h: head of the linked list
+ * Return: number of nodes
 */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return(count);
+}
