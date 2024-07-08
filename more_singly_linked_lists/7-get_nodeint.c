a0a6792487cce30f857d24398610825e264c7c7b
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-07 23:03:13 -0500

    file_8

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,14 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int n = 0;
+	unsigned int nth = 0;
 
 	while (head != NULL)
 	{
-		if (n == index)
+		if (nth == index)
 			return (head);
 		head = head->next;
-		n++;
+		nth++;
 	}
 	return (NULL);
 }
---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+/**
+ * sum_listint - returns the sum of all the data (n) of listint_t 
+ * @head: head of listint_t
+ * Return: Sum of all data in list. If empty, return 0
+*/
+int sum_listint(listint_t *head)
+{
+	unsigned int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
