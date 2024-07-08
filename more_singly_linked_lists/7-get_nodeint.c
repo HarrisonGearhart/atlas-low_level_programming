15c9b57bc2f035f45ae63a7c1db86b2701c57fe9
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-07 22:48:57 -0500

    file_7/jk, just needed == instead of =

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (head != NULL)
 	{
-		if (n = index)
+		if (n == index)
 			return (head);
 		head = head->next;
 		n++;
