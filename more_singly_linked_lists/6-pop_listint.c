67ca6c12b0667f05dfd38bb98d945b9d32bcaa7b
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-07 22:34:28 -0500

    file_6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * pop_listint - delets head of linked list
+ * @head: head of linked list
+ * Return: data (n) of head that was deleted
+*/
+int pop_listint(listint_t **head)
+{
+	int data = 0;
+	listint_t *temp;
+
+	if(*head == NULL)
+		return (0);
+	temp = *head;
+	data = (*head)->n;
+	*head = (*head)->next;
+	free(temp);
+
+	return(data);
+}
