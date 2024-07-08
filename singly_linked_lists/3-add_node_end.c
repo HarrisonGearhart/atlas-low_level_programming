dd11d53b75545c223dd773b25af68cf82ac1ae9a
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-07 23:12:38 -0500

    file_3/attempt2

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -7,11 +7,11 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	listint_t *new;
-	listint_t *temp;
+	list_t *new;
+	list_t *temp;
 
 	temp = *head;
-	new = (listint_t *)malloc(sizeof(listint_t));
+	new = (list_t *)malloc(sizeof(list_t));
 
 	if (!new)
 		return (NULL);
