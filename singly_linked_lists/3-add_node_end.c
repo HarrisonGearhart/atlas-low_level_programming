10aa05e769b9722014c51b4ffa6c3c05a5973b82
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-07 23:11:15 -0500

    file_3/attempt1

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	listint_t *new;
+	listint_t *temp;
+
+	temp = *head;
+	new = (listint_t *)malloc(sizeof(listint_t));
+
+	if (!new)
+		return (NULL);
+	new->str = strup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+	return (new);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string being counted
+ * Return: the length of the string
+*/
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
