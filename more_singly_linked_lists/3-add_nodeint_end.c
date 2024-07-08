7733aab05be6f89621d1c3531b06b513a47bc011
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-07 22:15:57 -0500

    file_3/attempt

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,5 +9,24 @@
 */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *new;
+	listint_t *temp;
 
+	temp = *head;
+	new = (listint_t *)malloc(sizeof(listint_t));
+
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+	return (new);
 }
