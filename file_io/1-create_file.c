55a0d2a3177be8ef836b8c2165152f8e004a5f17
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-08 22:24:12 -0500

    file_1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,9 +7,9 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int o = open(filename, O_CREATE | O_RDWR | O_TRUNC | 0600);
-	int w = write(o, text_content, len);
+	int o = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
 	int len = 0;
+	int w = write(o, text_content, len);
 
 	if (filename == NULL)
 		return (-1);
