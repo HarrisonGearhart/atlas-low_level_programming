614e02f2ff5d787fdee0bc6c6fc0849cfc4fdd27
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-09 14:47:07 -0500

    file_1/redo

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,11 +18,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		text_content = "";
-
-	for (len = 0; text_content[len]; len++)
-		;
-	
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 	w = write(o, text_content, len);
 	if (w == -1)
 		return (-1);
