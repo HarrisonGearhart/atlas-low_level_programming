b6197430176fdfe51f90f6de7fb61bbc3e3274fa
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-14 12:42:25 -0500

    file_4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees list
+ * @head: pointer to head of list
+ * Return: Void 
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current_node;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
