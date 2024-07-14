d44bb5c953934f09701fca467006cb5f3f36109d
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-14 12:36:27 -0500

    file_3/forgot->next

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	h = *head;
 	if (h != NULL)
 	{
-		while (h != NULL)
+		while (h->next != NULL)
 			h = h->next;
 		h->next = new;
 	}
