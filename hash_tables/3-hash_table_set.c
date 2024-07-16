148432941ce8d7495dd3081719e4789ceb002500
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-15 21:48:47 -0500

    file_3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -8,6 +8,7 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *new;
 	char *value_copy;
 	unsigned long int k_index, i;
 
@@ -28,4 +29,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+
+	new->key = strdup(key);
+	if (new->key == NULL);
+	{
+		free(new);
+		return(0);
+	}
+
+	new->value = value_copy;
+	new->next = ht->array[k_index];
+	ht->array[k_index] = new;
+	return (1);
 }
