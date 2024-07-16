39a5a9182c6fac81d8904a8125225ddca1af354d
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-15 21:49:24 -0500

    file_3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -38,7 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new->key = strdup(key);
-	if (new->key == NULL);
+	if (new->key == NULL)
 	{
 		free(new);
 		return(0);
