e863e60a012c0c697082b04223ccfd6f3acf74e7
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-15 22:06:22 -0500

    file_4/betty

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -3,14 +3,14 @@
  * hash_table_get - retrieves a value associated with a key
  * @ht: hash table
  * @key: key we're looking for
- * Return: the value associated with the element, NULL if not found 
+ * Return: the value associated with the element, NULL if not found
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int k_index;
 	hash_node_t *node;
 
-	if (ht == NULL || key == NULL || *key =='\0')
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	k_index = key_index((unsigned char *) key, ht->size);
