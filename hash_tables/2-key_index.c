28438b7c0f7905ade5758a062c8633a12b549d87
HarrisonGearhart <hgearhart98@gmail.com>
2024-07-15 21:12:47 -0500

    file_2/betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -2,7 +2,7 @@
 /**
  * key_index - gives the index of a key
  * @key: the key string
- * size: size of the array of the hash table
+ * @size: size of the array of the hash table
  * Return: the index position
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
