68e31993984b85275626649a53755fa94077da6e
HarrisonGearhart <hgearhart98@gmail.com>
2024-08-12 14:51:18 -0500

    added protos to header file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value)
+int binary_search_recursion(int *array, int left, int right, int value);
+void print_array(int *array, int left, int right);
 
 #endif
 
