901995484c4f869bcc02d7b267a22d734ca51a0d
HarrisonGearhart <hgearhart98@gmail.com>
2024-08-12 17:58:08 -0500

    update header

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -5,7 +5,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value)
-int binary_search_recursion(int *array, int left, int right, int value);
+int binary_search_recursive (int *array, int left, int right, int value);
 void print_array(int *array, int left, int right);
 
 #endif
